01679d5e40a12029a0b6cf3167ae428be04efa4c
Alon-Fabio <alon.the.fabio@gmail.com>
2019-11-17 18:33:52 +0200

    Updates

---
+++ precache-manifest.aa2f115c0e482efa597a5fde14698222.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e6003cf230283a99b698a224523712",
+    "url": "/KittyFriends/index.html"
+  },
+  {
+    "revision": "afdf72749299dea71b6a",
+    "url": "/KittyFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f0efe33eddf356af3b19",
+    "url": "/KittyFriends/static/css/main.cbd887e7.chunk.css"
+  },
+  {
+    "revision": "afdf72749299dea71b6a",
+    "url": "/KittyFriends/static/js/2.43ac5eff.chunk.js"
+  },
+  {
+    "revision": "f0efe33eddf356af3b19",
+    "url": "/KittyFriends/static/js/main.7e0d4769.chunk.js"
+  },
+  {
+    "revision": "a993099a7c23dd1d2c4c",
+    "url": "/KittyFriends/static/js/runtime-main.e5bd7070.js"
+  }
+]);
